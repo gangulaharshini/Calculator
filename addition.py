e2376e9bade8b026b31591545c67009c84216dba
Harshini <gangulaharshini7@gmail.com>
2024-03-19 17:01:22 -0500

    This is my first version

---
+++ addition.py
@@ -0,0 +1,14 @@
+ def add(num1, num2):
+    return num1 + num2
+
+def main():
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = add(num1, num2)
+        print("Result:", result)
+    except ValueError:
+        print("Please enter valid numbers.")
+
+if __name__ == "__main__":
+    main()
