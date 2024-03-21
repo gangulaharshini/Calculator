679c34c1bd52e0dd1a0fd7c490c24b00a59df960
Harshini <gangulaharshini7@gmail.com>
2024-03-20 23:02:31 -0500

    This is commit through vs code

--- modulus.py
+++ modulus.py
@@ -1,12 +1,13 @@
- // addition new num
- def add(num1, num2):
-    return num1 + num2
+def modulus(num1, num2):
+    if num2 == 0:
+        return "Error: Cannot divide by zero"
+    return num1 % num2
 
 def main():
     try:
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
-        result = add(num1, num2)
+        result = modulus(num1, num2)
         print("Result:", result)
     except ValueError:
         print("Please enter valid numbers.")
